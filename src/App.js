4873535955287a213e39339649123559f574cdad
Ушаков Дмитрий <neodmit@mail.ru>
2021-12-27 20:35:21 +0600

    Create project, add slide menu
    First of all i created project, deleted all useless files, set reset css link at header and  made side slide menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import MenuToggle from "./components/Navigation/MenuToggle/MenuToggle";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    menu: false
+  }
+  
+  toggleMenuHandler = () =>{
+    this.setState({
+      menu: !this.state.menu
+    })
+  }
+  
+  render() {
+      return (
+      <div className="App">
+        <MenuToggle 
+          onToggle = {this.toggleMenuHandler}
+          isOpen = {this.state.menu}
+        />
+        
+      </div>
+    );
+  }
 }
 
 export default App;
