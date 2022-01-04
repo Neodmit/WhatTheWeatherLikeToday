4a981854ab67a03e915a813ba38336d19c45a5f8
Ушаков Дмитрий <neodmit@mail.ru>
2022-01-04 14:40:53 +0600

    Change menu
    Change menu to driwer and add styles

--- src/App.js
+++ src/App.js
@@ -2,26 +2,13 @@ import React, {Component} from 'react';
 import MenuToggle from "./components/Navigation/MenuToggle/MenuToggle";
 
 class App extends Component {
-
-  state = {
-    menu: false
-  }
-  
-  toggleMenuHandler = () =>{
-    this.setState({
-      menu: !this.state.menu
-    })
-  }
-  
   render() {
       return (
-      <div className="App">
+
         <MenuToggle 
-          onToggle = {this.toggleMenuHandler}
-          isOpen = {this.state.menu}
+          
+      
         />
-        
-      </div>
     );
   }
 }
--- src/components/Navigation/MenuToggle/MenuToggle.js
+++ src/components/Navigation/MenuToggle/MenuToggle.js
@@ -1,30 +1,52 @@
-import classes from "./MenuToggle.module.css";
+import classes from "./MenuToggle.module.scss";
+import { Drawer, IconButton, List, ListItem , ListItemIcon, ListItemText} from '@material-ui/core'
+import React, {useState} from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+import menuItems from '../../../resources/menu/menuItems.json'
+import logo from '../pics/logo.svg'
 
 
-const MenuToggle = props =>{
-    
-    const cls = [
-        classes.MenuToggle,
-        'fa'
-    ]
-    
+const MenuToggle = () =>{
 
-    if (props.isOpen){
-        cls.push('fa-times')
-        cls.push(classes.open)
-    }else{
-        cls.push('fa-bars')
-    }
-
+    const [isOpen, setOpen] = useState(false);
     return( 
+        <>
+            <IconButton onClick={()=>setOpen(true)}>
+                <MenuIcon/>
+            </IconButton>
+           
+            <Drawer
+                 classes={{paper: classes["Menu-open"]}}
+                anchor={'left'}
+                open = {isOpen}
+                onClose={()=>setOpen(false)}
+  
+            >
+                <List>
+                    <div className={classes['Menu-header']}>
+                        <img src={logo} alt="Disaster" class="header-logo-pic"/>
+                        <h1  className={classes['Menu-header-text']}>Dashboard Kit</h1>
+                    </div>
+                    {setMenuItems()}
+                </List>
         
-        <i
-            className={cls.join(' ')}
-            onClick={props.onToggle}
-        />
-        
+            </Drawer>
+            
+        </>
     )
     
 }
+
+function setMenuItems(){
+    return(
+        menuItems.map((menuItems) => 
+        <ListItem divider={menuItems.divider} button  className={classes["Menu-items"]}>
+            <ListItemIcon > 
+                 <ListItemText classes={{primary: classes["Menu-item-text"]}}>{menuItems.name}</ListItemText>
+            </ListItemIcon>
+        </ListItem>)
+    );
+}
+
 
 export default MenuToggle
